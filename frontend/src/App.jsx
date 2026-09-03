b2cb0e44d8d81a12623ebe564d7b4912f7c3c5ef
ABHISHEK KUMAR RAJ <abhishekraj93088@gmail.com>
2026-09-03 11:36:42 +0530

    update

--- frontend/src/App.jsx
+++ frontend/src/App.jsx
@@ -1,10 +1,24 @@
+import Doctors from "./components/doctors/Doctors.jsx";
 import Footer from "./components/footer/Footer.jsx";
 import Header from "./components/Header/Header.jsx";
+import Home from "./components/home/Home.jsx";
 
 function App() {
   return(
     <>
       <Header />
+      <main>
+
+        <section id="home">
+          <Home />
+        </section>
+
+        <section id="doctors">
+          <Doctors />
+        </section>
+
+
+      </main>
       <Footer />
     </>
   )
